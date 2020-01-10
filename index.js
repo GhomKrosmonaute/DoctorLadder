ffc26c9a217f64bf21474ef45c7232d8db2f7a6c
CamilleAbella <camille.abella@hotmail.fr>
2020-01-10 23:14:34 +0100

    fix all bugs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Score = require('discord-scores')
 
 const db = new Pool(config.database)
 const client = new Client()
-const scores = new Score(client,[
+const scores = new Score( client, [
     {'507420549765529610':2},
     {'👎':-1},
     {'👍':1}
@@ -17,17 +17,37 @@ db.connect()
 client.login(config.token)
 
 client.once( 'ready', yay => {
-    console.log('Bot is ready', yay)
+    console.log('Bot is ready')
+    client.ready = true
 })
 
-scores.on('add', event => changeScore(event, event.value > 0 ? '+' : '-'))
-scores.on('remove', event => changeScore(event, event.value > 0 ? '-' : '+'))
+client.on( 'message', message => {
 
-function changeScore(event, sign){
+    if(message.system || message.author.bot) return
+
+    if(/^(?:top|ladder|classement|helping|scores?|points?)$/i.test(message.content)){
+        db.query(`SELECT * FROM "user" ORDER BY helping_points DESC LIMIT 10`)
+            .then(res => {
+                message.channel.send(JSON.stringify(res.rows, null, 2))
+            })
+            .catch(console.error)
+    }
+
+})
+
+scores.on('add', event => updateScore(event, event.value > 0 ? '+' : '-'))
+scores.on('remove', event => updateScore(event, event.value > 0 ? '-' : '+'))
+
+function updateScore(event, sign){
     const value = String(event.value).replace('-','')
     db.query({
-        text: 'UPDATE `score` SET `point` = `point` ' + sign + ' $1 WHERE `user_id` = $2',
-        values: [ value, event.user.id ]
+        text: `
+            INSERT INTO "user" ( member_id, helping_points )
+            VALUES ( $2, ${sign=='+'?'':sign}$1 )
+            ON CONFLICT ( member_id )
+            DO UPDATE SET helping_points = Excluded.helping_points ${sign} $1
+        `,
+        values: [ value, event.message.author.id ]
     })
         .then(res => event.message.react('👀').catch(err=>{}))
         .catch(console.error)
