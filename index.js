f67c723ce178deaddc3a8f0609dddb8feeb541e2
CamilleAbella <camille.abella@hotmail.fr>
2020-01-11 12:16:34 +0100

    add top embed and connect to real db

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 const config = require('./config.json')
 
-const { Pool } = require('pg')
-const { Client } = require('discord.js')
+const pg = require('pg')
 const Score = require('discord-scores')
+const { Client, RichEmbed } = require('discord.js')
 
-const db = new Pool(config.database)
+pg.defaults.parseInt8
+
+const db = new pg.Pool(config.database)
 const client = new Client()
 const scores = new Score( client, [
     {'507420549765529610':2},
@@ -17,38 +19,86 @@ db.connect()
 client.login(config.token)
 
 client.once( 'ready', yay => {
+    client.labs = client.guilds.first()
+    client.labs.gif = client.labs.iconURL.replace(/(jpe?g|png)$/,'gif')
     console.log('Bot is ready')
-    client.ready = true
 })
 
 client.on( 'message', message => {
 
     if(message.system || message.author.bot) return
 
+    const { labs } = client
+
     if(/^(?:top|ladder|classement|helping|scores?|points?)$/i.test(message.content)){
-        db.query(`SELECT * FROM "user" ORDER BY helping_points DESC LIMIT 10`)
+        db.query(`
+            SELECT u.id, SUM(hp.value) AS "points"
+            FROM "user" u
+            LEFT JOIN "helping_points" hp
+            ON hp.user_id = u.id
+            GROUP BY u.id
+            ORDER BY "points" DESC LIMIT 10
+        `)
             .then(res => {
-                message.channel.send(JSON.stringify(res.rows, null, 2))
+
+                let embed = new RichEmbed()
+                    .setAuthor('Top 10 Helpers', labs.gif)
+
+                if(res.rows.length > 0){
+
+                    const names = res.rows.map(row => labs.members.has(row.id) ? labs.members.get(row.id).displayName.slice(0,15) : row.id)
+                    const points = res.rows.map(row => row.points)
+                    const ranks = ['🥇','🥈','🥉',4,5,6,7,8,9,10].slice(0,res.rows.length)
+
+                    const rdm = Math.floor(Math.random(Math.min(res.rows.length,3)))
+                    client.user.setActivity(`le top 3 |\n${ranks[rdm]} ${points[rdm]}pts ${names[rdm]}`, {type:"WATCHING"})
+
+                    embed.addField('#', ranks.join('\n'), true)
+                    embed.addField('Pts.', points.join('\n'), true)
+                    embed.addField('Names', names.join('\n'), true)
+
+                }else{
+                    embed.setDescription('❌ Aucun résultat...')
+                }
+                message.channel.send(embed)
             })
-            .catch(console.error)
+            .catch(err => {
+                message.channel.send(err.message)
+            })
     }
 
 })
 
-scores.on('add', event => updateScore(event, event.value > 0 ? '+' : '-'))
-scores.on('remove', event => updateScore(event, event.value > 0 ? '-' : '+'))
+scores.on('add', event => updateScore(event, event.value))
+scores.on('remove', event => updateScore(event, event.value * -1))
 
-function updateScore(event, sign){
-    const value = String(event.value).replace('-','')
-    db.query({
-        text: `
-            INSERT INTO "user" ( member_id, helping_points )
-            VALUES ( $2, ${sign=='+'?'':sign}$1 )
-            ON CONFLICT ( member_id )
-            DO UPDATE SET helping_points = Excluded.helping_points ${sign} $1
-        `,
-        values: [ value, event.message.author.id ]
-    })
-        .then(res => event.message.react('👀').catch(err=>{}))
-        .catch(console.error)
+function updateScore(event, value){
+    db.query(`
+        INSERT INTO "user" ( id )
+        VALUES ( ${event.message.author.id} )
+        ON CONFLICT DO NOTHING
+    `)
+        .then(done => {
+            db.query({
+                text: `
+                    INSERT INTO "helping_points" 
+                    ( user_id, author_id, value )
+                    VALUES ( 
+                        ${event.message.author.id},
+                        ${event.user.id},
+                        $1
+                    )
+                `,
+                values: [ value ]
+            })
+                .then(res => {
+                    event.message.react('👀')
+                        .then(reac => {
+                            setTimeout(()=>{
+                                reac.remove().catch(err=>{})
+                            }, 2000)
+                        }).catch(err=>{})
+                }).catch(console.error)
+        }).catch(console.error)
+        
 }
